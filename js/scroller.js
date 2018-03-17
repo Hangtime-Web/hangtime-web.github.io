092fd8312ab770c4275c7d7bb4518d235b65829d
Avner Khan <avnerkhan@Avners-MBP.attlocal.net>
2018-03-17 16:34:17 -0500

    functional subscribe button

--- js/scroller.js
+++ js/scroller.js
@@ -2,3 +2,14 @@ function givealert(message)
 {
   window.alert(message);
 }
+function subscribeAdd()
+{
+  var sub = document.getElementById("subscribe").value;
+  var data = {
+    email: sub
+  }
+  var firebaseRef = firebase.database();
+  firebaseRef.ref("subs").push(data);
+  givealert("You have successfully subscribed for future hangtime updates.");
+
+}
