8d1058c918095dec49e81ddc76656dd0f41c4a8e
Jerry Huang <jerry@aplomb.co>
2018-04-11 13:03:18 -0500

    Updated team bio's

--- js/scroller.js
+++ js/scroller.js
@@ -2,14 +2,3 @@ function givealert(message)
 {
   window.alert(message);
 }
-function subscribeAdd()
-{
-  var sub = document.getElementById("subscribe").value;
-  var data = {
-    email: sub
-  }
-  var firebaseRef = firebase.database();
-  firebaseRef.ref("subs").push(data);
-  givealert("You have successfully subscribed for future hangtime updates.");
-
-}
