ccd1d53575286f719191150fcb6a94bcf7398e8a
Avner Khan <avnerkhan@Avners-MBP.attlocal.net>
2018-08-06 20:14:12 -0500

    New Website Overhaul

--- js/main.js
+++ js/main.js
@@ -1,106 +1,44 @@
-jQuery(document).ready(function(){
-    "use strict";
-//    $('#slider-carousel').carouFredSel({
-//        responsive:true,
-//        width:'100%;',
-//        circular:true,
-//        
-//        scroll:
-//               {
-//                 items:1,
-//                 duration:500,
-//                 pouseOnHover:true
-//                  },
-//                auto:true,
-//                items:
-//                        
-//                        {
-//                         visible:
-//                                       {
-//                                       min:1,
-//                                       max:1
-//                                       },
-//                                       height:"variable"
-//                        },
-//        
-//        
-//                pagination:
-//                            {
-//                              container:".sliderpager",         
-//                              pageAnchorBuilder:false
-//                            },
-//        
-//    });
-    
-    $('.portfolio-carousel').carouFredSel({
-        responsive:true,
-        width:'100%;',
-        circular:true,
-        prev: '#prev', 
-        next: '#next',
-        
-        scroll:
-               {
-                 items:1,
-                 duration:500,
-                 pouseOnHover:true
-                  },
-                auto:true,
-                items:
-                        
-                        {
-                         visible:
-                                       {
-                                       min:1,
-                                       max:4
-                                       },
-                                       height:"variable"
-                        },
-        
-    });
-    
-    $(window).scroll(function(){
-       
-        var top=$(window).scrollTop();
-        
-        if(top>=100) {
-            $("header").addClass('secondary');
-        }
-        
-        else if ($("header").hasClass('secondary')){
-            
-                 $("header").removeClass('secondary');
-            
-                 }
-    });
-    
-    $('#menu').slicknav({
-        label:'',
-    })
-    
-    
-    // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-    
-    
+$(window).load(function() {
+    $("#preloader-wrapper").fadeOut("slow");
 });
+
+$(document).ready(function(){
+
+
+     
+    //animated header class
+    $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll > 200) {
+        //console.log('a');
+        $(".navigation").addClass("animated");
+    } else {
+        //console.log('a');
+        $(".navigation").removeClass("animated");
+    }});
+
+
+
+    $(".gallery-slider").owlCarousel(
+        {
+        pagination : true,
+        autoPlay : 5000,
+        itemsDesktop  :  [1500,4],
+        itemsDesktopSmall :  [979,3]
+        }
+    );
+
+    // Gallery Popup
+    $('.image-popup').magnificPopup({type:'image'});
+
+
+
+});
+
+
+
+
+
+
+
--- js/scroller.js
+++
@@ -1,4 +0,0 @@
-function givealert(message)
-{
-  window.alert(message);
-}
